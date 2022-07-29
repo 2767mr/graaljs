6bc4fc100b029dc99ebc3ce8e9cc73090a4eafe9
Julian Garn <julian.garn@gmx.at>
2022-07-29 09:10:30 +0200

    Remove unused method

--- graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/nodes/access/IteratorStepNode.java
+++ graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/nodes/access/IteratorStepNode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -62,15 +62,6 @@ public class IteratorStepNode extends JavaScriptBaseNode {
 
     public Object execute(IteratorRecord iteratorRecord) {
         Object result = iteratorNextNode.execute(iteratorRecord);
-        Object done = iteratorCompleteNode.execute(result);
-        if (done == Boolean.TRUE) {
-            return false;
-        }
-        return result;
-    }
-
-    public Object execute(IteratorRecord iteratorRecord, Object value) {
-        Object result = iteratorNextNode.execute(iteratorRecord, value);
         Object done = iteratorCompleteNode.execute(result);
         if (done == Boolean.TRUE) {
             return false;
